2871c02d4dde8cb992c05b98b6b03dd9743a7063
park-protectors <janiehewitt09@gmail.com>
2026-01-05 18:31:59 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,23 @@
-self.addEventListener("fetch", function(event) {});
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('park-protectors-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/missions.html',
+        '/leaderboard.html',
+        '/parks.html',
+        '/style.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
